76ce041961dccfde3c90c71a2c91cb3e097475a8
matteo-d <matteodrouin33@gmail.com>
2023-09-20 17:17:24 +0200

    Get Balances

--- getBalance.py
+++ getBalance.py
@@ -1,4 +1,4 @@
-from config import BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_API_KEY_INTRA, BYBIT_API_SECRET_INTRA
+from config import BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_API_KEY_INTRA, BYBIT_API_SECRET_INTRA, BYBIT_API_KEY_INTRA2, BYBIT_API_SECRET_INTRA2, BYBIT_API_KEY_INTRA3, BYBIT_API_SECRET_INTRA3, BYBIT_API_KEY_CLASSIC, BYBIT_API_SECRET_CLASSIC, BYBIT_API_KEY_CLASSIC2, BYBIT_API_SECRET_CLASSIC2,BYBIT_API_KEY_CLASSIC3, BYBIT_API_SECRET_CLASSIC3, BYBIT_API_KEY_SWING, BYBIT_API_SECRET_SWING, BYBIT_API_KEY_SWING2, BYBIT_API_SECRET_SWING2, BYBIT_API_KEY_SWING3, BYBIT_API_SECRET_SWING3
 from pybit.unified_trading import HTTP
 
 session = HTTP(
@@ -6,14 +6,62 @@ session = HTTP(
     api_key=BYBIT_API_KEY,
     api_secret=BYBIT_API_SECRET,
 )
-sessionOne = HTTP(
+sessionIntra = HTTP(
     testnet=False,
     api_key=BYBIT_API_KEY_INTRA,
     api_secret=BYBIT_API_SECRET_INTRA,
 )
+sessionIntra2 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_INTRA2,
+    api_secret=BYBIT_API_SECRET_INTRA2,
+)
+sessionIntra3 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_INTRA3,
+    api_secret=BYBIT_API_SECRET_INTRA3,
+)
+sessionClassic = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_CLASSIC,
+    api_secret=BYBIT_API_SECRET_CLASSIC,
+)
+sessionClassic2 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_CLASSIC2,
+    api_secret=BYBIT_API_SECRET_CLASSIC2,
+)
+sessionClassic3 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_CLASSIC3,
+    api_secret=BYBIT_API_SECRET_CLASSIC3,
+)
+sessionSwing = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_SWING,
+    api_secret=BYBIT_API_SECRET_SWING,
+)
+sessionSwing2 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_SWING2,
+    api_secret=BYBIT_API_SECRET_SWING2,
+)
+sessionSwing3 = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_SWING3,
+    api_secret=BYBIT_API_SECRET_SWING3,
+)
 
 print(session.get_wallet_balance(accountType="SPOT")['result']['list'][0]['coin'][1]['walletBalance']+" $")
 
-print(sessionOne.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionIntra.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionIntra2.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionIntra3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionClassic.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionClassic2.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionClassic3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionSwing2.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionSwing3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
+print(sessionSwing3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
 
 exit()
