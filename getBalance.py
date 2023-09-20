71296c2221a937889e067ab4126a48c6bd97fda1
matteo-d <matteodrouin33@gmail.com>
2023-09-20 16:30:15 +0200

    Get Balance subaccount

--- getBalance.py
+++ getBalance.py
@@ -1,4 +1,4 @@
-from config import BYBIT_API_KEY, BYBIT_API_SECRET
+from config import BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_API_KEY_INTRA, BYBIT_API_SECRET_INTRA
 from pybit.unified_trading import HTTP
 
 session = HTTP(
@@ -6,9 +6,14 @@ session = HTTP(
     api_key=BYBIT_API_KEY,
     api_secret=BYBIT_API_SECRET,
 )
+sessionOne = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY_INTRA,
+    api_secret=BYBIT_API_SECRET_INTRA,
+)
 
-print(session.get_wallet_balance(
-    accountType="SPOT"
-)['result']['list'][0]['coin'][1]['walletBalance']+" $")
+print(session.get_wallet_balance(accountType="SPOT")['result']['list'][0]['coin'][1]['walletBalance']+" $")
+
+print(sessionOne.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity']+" S")
 
 exit()
